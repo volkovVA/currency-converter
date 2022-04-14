d5d7b91ed53747518195584340ae8efd9d06870e
volkovVA <volkov.slava@hotmail.com>
2022-04-14 21:52:02 +0300

    refactor: changed currency output

--- src/components/CurrencyOutput/CurrencyOutput.jsx
+++ src/components/CurrencyOutput/CurrencyOutput.jsx
@@ -8,66 +8,61 @@ function CurrencyOutput() {
   );
 
   const getName = (code) => {
-    if (currencySupported && currencyConversion) {
-      return currencySupported.find(
-        (el) => el.currency.code === currencyConversion[code]
-      ).currency.name;
-    }
+    return currencySupported.find(
+      (el) => el.currency.code === currencyConversion[code]
+    ).currency.name;
   };
 
   const getNumber = () => {
-    if (currencyConversion) {
-      const arrayOfNumber = [
-        ...currencyConversion.conversion_result.toFixed(6).toString(),
-      ];
-      const number1 = arrayOfNumber.slice(0, 4).join('');
-      const number2 = arrayOfNumber.slice(4).join('');
-      return [number1, number2];
-    }
+    const arrayOfNumber = currencyConversion.conversion_result
+      .toFixed(6)
+      .toString()
+      .split('.');
+    const number1 = arrayOfNumber[0];
+    const number2 = [...arrayOfNumber[1]].slice(0, 2).join('');
+    const number3 = [...arrayOfNumber[1]].slice(2).join('');
+
+    return [number1, number2, number3];
   };
 
-  return (
-    <div className="output">
-      <div>
-        <p className="output-base">
-          {currencyConversion && currencyConversion.amount}.00{' '}
-          {getName('base_code')} =
-        </p>
-        <p className="output-target">
-          {currencyConversion && getNumber()[0]}
-          <span className="output-number">
-            {currencyConversion && getNumber()[1]}{' '}
-          </span>
-          {getName('target_code')}
-        </p>
-      </div>
-      <div className="output-info">
-        <div className="output-one">
-          <p>
-            1 {currencyConversion && currencyConversion.base_code} ={' '}
-            {currencyConversion &&
-              currencyConversion.conversion_rate.toFixed(4)}{' '}
-            {currencyConversion && currencyConversion.target_code}
+  if (currencyConversion) {
+    return (
+      <div className="output">
+        <div>
+          <p className="output-base">
+            {currencyConversion.amount}.00&nbsp;{getName('base_code')}&nbsp;=
           </p>
-          <p>
-            1 {currencyConversion && currencyConversion.target_code} ={' '}
-            {currencyConversion &&
-              (1 / currencyConversion.conversion_rate).toFixed(4)}{' '}
-            {currencyConversion && currencyConversion.base_code}
+          <p className="output-target">
+            {getNumber()[0]}.{getNumber()[1]}
+            <span className="output-number">{getNumber()[2]}&nbsp;</span>
+            {getName('target_code')}
           </p>
         </div>
-        <div className="output-date">
-          <span>{getName('base_code')}</span> to
-          <span> {getName('target_code')}</span> conversion — Last updated
-          {currencyConversion &&
-            currencyConversion.time_next_update_utc
+        <div className="output-info">
+          <div className="output-one">
+            <p>
+              1&nbsp;{currencyConversion.base_code}&nbsp;=&nbsp;
+              {currencyConversion.conversion_rate.toFixed(4)}&nbsp;
+              {currencyConversion.target_code}
+            </p>
+            <p>
+              1&nbsp;{currencyConversion.target_code}&nbsp;=&nbsp;
+              {(1 / currencyConversion.conversion_rate).toFixed(4)}&nbsp;
+              {currencyConversion.base_code}
+            </p>
+          </div>
+          <div className="output-date">
+            <span>{getName('base_code')} </span>to
+            <span> {getName('target_code')}</span> conversion — Last updated
+            {currencyConversion.time_next_update_utc
               .split(' ')
               .slice(0, -1)
               .join(' ')}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CurrencyOutput;

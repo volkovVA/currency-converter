5d5d2bb3eaa3a7efcf7e6b11f23107d4d596efd3
Volkov Slava <volkov.frontend@gmail.com>
2024-05-05 18:23:29 +0300

    feat: migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  envPrefix: 'REACT_APP_',
+
+  plugins: [react()],
+  build: {
+      outDir: "build",
+  },
+  server: {
+      port: 4000,
+      open: true,
+  },
+});
